16da2c4fed583296bfa5b8320935a8def147692a
lwais3 <wa7shkasr@gmail.com>
2022-09-11 07:13:01 +0300

    Upload The Design

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 // 
 let plans_i = document.querySelectorAll(".mumber-plan .holder .checbox");
-let planHolder = document.querySelectorAll(".mumber-plan .plan")
-
 plans_i.forEach(function(e){
   e.onclick = function() {
     plans_i.forEach(function(e){
